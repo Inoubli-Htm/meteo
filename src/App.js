7a42e53aa610e9b8a94b10f00191c75445378800
Inoubli-Htm <inoubli.h@gmail.com>
2022-10-12 16:43:39 +0000

    test meteo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const APIKEY = "f825344b0cf0672c689378549f9868db";
+
+export default function App() {
+  const [ville, setVille] = useState("");
+  const [resultat, setResultat] = useState({});
+
+  const gettemperature = async (e) => {
+    e.preventDefault();
+    if (!ville) {
+      return;
+    }
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${APIKEY}`
+    );
+    const { main } = await res.json();
+
+    setResultat(main);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        margin: "auto",
+        width: "30%",
+        marginTop: "10px",
+        marginBottom: "10px",
+      }}
+    >
+      <form onSubmit={gettemperature}>
+        <div
+          style={{
+            marginTop: "10px",
+            marginBottom: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Form.Control
+            type="search"
+            placeholder="Nom de la ville"
+            className="me-2"
+            aria-label="Search"
+            value={ville}
+            onChange={(e) => setVille(e.target.value)}
+          />
+        </div>
+        <div
+          style={{
+            marginTop: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <Button variant="primary" type="submit">
+            Verifier la temperature
+          </Button>{" "}
+        </div>
+      </form>
+      {resultat && (
+        <div>
+          <p>la temperature est de {parseInt(resultat.temp - 273.15)} °C</p>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
